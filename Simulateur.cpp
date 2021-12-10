140129e49b8c06af478aeb4ca29820b558b8644d
my1meg <megimyftaraj123@yahoo.com>
2021-12-10 13:33:25 +0100

    simulateur.cpp

--- Simulateur.cpp
+++ Simulateur.cpp
@@ -17,11 +17,10 @@ int resultat Simulateur(vector<Noeud> &Noeud_vector)
   // Un iterateur va recuperer l'info de chaque Noeud du vector_Noeud
 
 
-  // On lit chaque noued
-  // Si le type c'est input, on appelle set_logic set_logic_state
-  // Si le type est un output, on stocke la valeur dans resultat
-  // S'il s'agit d'un gate, on effectue l'operation entre les noeud dans les adresses qui le precedent et on stocke le resultat dans le logic state du Noeud qui le suit
-
+  // On lit chaque noeud
+  // Si le type c'est input              val logique get.valeur== 0  ou 1
+  // Si le type est un output              val logique inconnue get.valeur==2
+  // S'il s'agit d'un gate                val logique inconnue get.valeur==2
 
 
   for(std::vector<Noeud>::iterator it = Noeud_vector.begin(); it != Noeud_vector.end(); ++it)
