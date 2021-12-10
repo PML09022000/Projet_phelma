861db5cb5aa9724a008371fb0e3b93c9b8aaefcc
Paul-Arthur <paularthur.mehl@gmail.com>
2021-12-10 15:00:41 +0100

    1er commit parser_structure

--- parser.cpp
+++ parser.cpp
@@ -13,6 +13,7 @@
 using namespace std;
 
 static bool parser_decoupage(vector<Symbole> &symbole_vector);
+static vector<Noeud> parser_structure(vector<Symbole> &symbole_vector);
 
 void parser(vector<Symbole> &symbole_vector){
 
@@ -20,8 +21,51 @@ void parser(vector<Symbole> &symbole_vector){
                                                           // On doit verifier l'ordre
                                                           // On parcoure ce vecteur pour verifier l'ordre des symboles
 
+    for(std::vector<Symbole>::iterator it = symbole_vector.begin(); it != symbole_vector.end();) // suppression des symboles de nature ponctuation
+    {
+      if((*it).get_nature() == ponctuation){
+        it = symbole_vector.erase(it);
+      }
+      else{
+        ++it;
+      }
+    }
+
+    vector<Noeud> noeud_vector = parser_structure(symbole_vector);
 
   }
+
+vector<Noeud> parser_structure(vector<Symbole> &symbole_vector){
+    vector<Noeud> noeud_vector;
+    vector<string> nom_noeud_vector;
+    for(std::vector<Symbole>::iterator it1 = symbole_vector.begin(); it1 != symbole_vector.end(); ++it1){
+      if((*it1).get_nature() == identifiant){
+        string current_identifiant = (*it1).get_valeur();
+        bool existant = false;
+        for(std::vector<string>::iterator it2 = nom_noeud_vector.begin(); it2 != nom_noeud_vector.end(); ++it2){
+          if((*it2) == current_identifiant){
+            existant = true;
+          }else{
+
+          }
+        }
+        if(existant == false){
+          nom_noeud_vector.push_back(current_identifiant);
+          Noeud new_noeud();
+        }else{
+          cout << "nom deja trouve : " << current_identifiant << endl;
+          if((*(it1+3)).get_nature() != mot_clef){
+            cout << "OK" << endl;
+          }else{
+            cout << "ERROR" << endl;
+          }
+        }
+      }else{
+
+      }
+    }
+    return noeud_vector;
+}
 
 
 
